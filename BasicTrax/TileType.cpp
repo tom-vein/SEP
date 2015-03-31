e516d61140e332e7846de95d9d9506afbe4cdb53
Mario Theuermann <mario.theuermann@student.tugraz.at>
2015-03-31 15:48:23 +0200

    added implementation for calculating bottum color of cross shape

--- BasicTrax/TileType.cpp
+++ BasicTrax/TileType.cpp
@@ -6,10 +6,41 @@ TileType::TileType(Shape shape, Color top_color) : shape_(shape),
   top_color_(top_color)
 {
 }
-Shape TileType::getShape() const;
-Color TileType::getTopColor() const;
-Color TileType::getBottomColor() const;
-Color TileType::getRightColor() const;
-Color TileType::getLeftColor() const;
-void TileType::initTileTypes();
-const TileType& TileType::getTileType(Shape shape, Color top_color);
+
+Shape TileType::getShape() const
+{
+  return shape_;
+}
+
+Color TileType::getTopColor() const
+{
+  return top_color_;
+}
+
+Color TileType::getBottomColor() const
+{
+  switch(shape_)
+  {
+    case Shape::CROSS:
+      return (TileType::top_color_ = Color::WHITE) ? Color::WHITE : Color::RED;
+  }
+}
+
+Color TileType::getRightColor() const
+{
+}
+
+Color TileType::getLeftColor() const
+{
+}
+
+void TileType::initTileTypes()
+{
+  //erzeugt die 6 arten der tiles (vector hinzufügen)
+}
+
+const TileType& TileType::getTileType(Shape shape, Color top_color)
+{
+  //dementsprechenden tile aus dem vector zurückgeben
+}
+
