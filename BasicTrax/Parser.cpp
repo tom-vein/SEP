2b6631dbfde1b23d15b9980c5b0dfd9b2c4ed1df
Mario Theuermann <mario.theuermann@student.tugraz.at>
2015-04-14 08:45:24 +0200

    parser.cpp tried to return arguments from parseArguments

--- BasicTrax/Parser.cpp
+++ BasicTrax/Parser.cpp
@@ -1,4 +1,5 @@
 #include "Parser.h"
+#include <string>
 
 std::shared_ptr<CommandLib::Command> Parser::parseCommand(
     const std::string& command_string)
@@ -8,5 +9,15 @@ std::shared_ptr<CommandLib::Command> Parser::parseCommand(
 
 std::string Parser::parseArguments(int argc, char* argv[])
 {
-
+  if(argc == 1 || argc == 3)
+  {
+    std::vector<std::string> all_args(argv, argv + argc);
+    return all_args;
+  }
+  else
+  {
+    std::string errorstring("Wrong Number of Arguments");
+    CommandLib::ErrorCommand(errorstring);
+    //return?
+  }
 }
