8eec8657ebe5db775c6eb9f84e11dbbae5cf5eb8
Stephan Valentan <stephan.valentan@posteo.at>
2015-05-29 23:26:24 +0200

    max tiles back to 64

--- BasicTrax/Game.h
+++ BasicTrax/Game.h
@@ -45,7 +45,7 @@ namespace GameLib
     //--------------------------------------------------------------------------
     // Constant which indicates the maximum number of tiles
     //
-    static const unsigned int MAX_OF_TILES = 5;
+    static const unsigned int MAX_OF_TILES = 64;
 
     //--------------------------------------------------------------------------
     // Logs the number of all placed tiles in the current turn. This includes
